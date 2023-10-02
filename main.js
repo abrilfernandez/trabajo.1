2f765ac789ce3254a62575a145281e4193dbd929
abrilfernandez <109990436+abrilfernandez@users.noreply.github.com>
2023-10-02 11:22:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+alert ("bienvenidos a la escuela nuestra senora de lourdes"); 
+let nota1;
+let nota2;
+let promedio;
+do {nota1=parseFloat(prompt("ingresa la primer nota"));
+nota2=parseFloat(prompt("ingresa la segunda nota"));
+let resultado=(nota1+nota2)/2;
+if (resultado >= 7) {
+   alert("Su promedio es de " + resultado + ". Aprobado")}
+   else if (resultado <= 6) { 
+   alert ("Su promedio es de " + resultado + ". Desaprobado.")}
+   else { 
+   alert("Por favor ingresar solo números")
+   }}
+while(isNaN(resultado), nota1 ===,nota2===){
+
+}
+function resultado (){
+alert("nota1"+"nota2") 
+return resultado
+}
+
+
